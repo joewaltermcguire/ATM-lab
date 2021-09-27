e66549e57c884bc701755972cb0697b09360688a
Joseph Mcguire <joemcguirekc@gmail.com>
2021-09-27 12:02:03 -0500

    Adding Fruitloops 1 to main

---
+++ CSF1Homework/FruitLoop1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class FruitLoop1
+    {
+        static void Main(string[] args)
+        {
+
+            string[] fruit = new string[6];
+            fruit[0] = "mangos";//in the drawer with an index of 0, put "mangos"
+            fruit[1] = "apples";
+            fruit[2] = "kiwis";
+            fruit[3] = "pineapples";
+            fruit[4] = "oranges";
+            fruit[5] = "papayas";
+
+            
+           
+                for (int i = 0; i < fruit.Length; i++)
+                {
+                Console.WriteLine(fruit[i]);
+                }
+               
+           
+        }
+    }
+}
